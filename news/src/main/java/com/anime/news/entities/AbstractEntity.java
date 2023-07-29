10b3c6e14d69e0b086889d57f883da8815248ffc
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-29 09:02:02 -0300

    Add AbstractEntity como uma super class

---
+++ news/src/main/java/com/anime/news/entities/AbstractEntity.java
@@ -0,0 +1,24 @@
+package com.anime.news.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@MappedSuperclass
+@Data
+public class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+}
